914e5fb8d736dd0bf1bd311f18bff7729b1e8a1b
Neidhardt <icefooooooooox@gmail.com>
2019-11-18 16:03:25 +0800

    fix import

--- decision_tree/decision_tree_classifier.py
+++ decision_tree/decision_tree_classifier.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn import tree
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_recall_curve  
 from sklearn.metrics import classification_report 
 
